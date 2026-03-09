203cd9c76b0c46a07e69e7e3369ad06894b06503
baccu447 <mycoursestore123@gmail.com>
2026-03-09 03:25:06 +0000

    Build: Generate official Flutter project structure (android, ios, web, etc.)

---
+++ android/app/src/main/kotlin/com/example/smart_cash_ledger_mobile_v2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.smart_cash_ledger_mobile_v2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import isar_flutter_libs
+import webview_flutter_wkwebview
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  IsarFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "IsarFlutterLibsPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
+}
